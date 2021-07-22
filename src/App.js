61062f183d98a88a123560e623c4d6d3ad857894
mdarora <mdarora0001@gmail.com>
2021-07-22 22:53:41 +0530

    now UI done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import {useState} from 'react'
+const initialGameState = ["","","","","","","","",""];
 
 const App = () => {
+
+  const [gameState, setgameState] = useState(initialGameState);
+
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
       <div className="square-container">
-        <div className="squares">1</div>
-        <div className="squares">2</div>
-        <div className="squares">3</div>
-        <div className="squares">4</div>
-        <div className="squares">5</div>
-        <div className="squares">6</div>
-        <div className="squares">7</div>
-        <div className="squares">8</div>
-        <div className="squares">9</div>
+        <div className="squares">{gameState[0]}</div>
+        <div className="squares">{gameState[1]}</div>
+        <div className="squares">{gameState[2]}</div>
+        <div className="squares">{gameState[3]}</div>
+        <div className="squares">{gameState[4]}</div>
+        <div className="squares">{gameState[5]}</div>
+        <div className="squares">{gameState[6]}</div>
+        <div className="squares">{gameState[7]}</div>
+        <div className="squares">{gameState[8]}</div>
+      </div>
+      <div className="clear-btn">
+        <button onClick={() => setgameState(initialGameState)}>Clear Game</button>
       </div>
     </div>
   )
